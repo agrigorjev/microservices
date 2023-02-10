19d63b5aadf18bb79d738f65874314edf6982bd6
eugin <eugin@DESKTOP-EDJCROV>
2023-02-10 18:50:05 +0300

    Missed on l;iast commit

---
+++ calendars-service/Mandara.CalendarsServiceTests/GrpcServices/CalendarsGrpcServiceTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mandara.CalendarsService.GrpcServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mandara.CalendarsService.GrpcServices.Tests
+{
+    [TestClass()]
+    public class CalendarsGrpcServiceTests
+    {
+        [TestMethod()]
+        public void CalendarsGrpcServiceTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetAllStockCalendarsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetStockCalendarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetAllHolidaysTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetHolidaysTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetAllExpiryDatesTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetExpiryDatesTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ calendars-service/Mandara.CalendarsServiceTests/Service/Startup.cs
@@ -0,0 +1,38 @@
+﻿using Mandara.CalendarsService.GrpcServices;
+using Mandara.CalendarsService.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mandara.CalendarsGrpcTests.Service
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddGrpc(o => o.EnableDetailedErrors = true);
+            services.AddSingleton<ICalendarsStorage, CalendarsStorage>();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGrpcService<CalendarsGrpcService>();
+            });
+        }
+    }
+}
