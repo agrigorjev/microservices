b2c3c6a261103567ecbf54638cfe7f379abafa11
Igor Barchevskiy <igor@roxosoft.com>
2023-03-17 17:26:33 +0300

    For new GUI

---
+++ products-gui-new/test/MonthlySpread.cs
@@ -0,0 +1,15 @@
+﻿using Mandara.Entities.ProductDefinition;
+
+namespace Mandara.RiskMgmtTool.Spreader.MonthlySpreader
+{
+    public class MonthlySpread
+    {
+        public DateString Month { get; set; }
+        public decimal? FuturesEquivalent { get; set; }
+        public string Spreads { get; set; }
+        public decimal JALSpreads { get; set; }
+
+        public decimal BookFuturesEquivalent { get; set; }
+        public decimal TotalJALSpreads { get; set; }
+    }
+}
